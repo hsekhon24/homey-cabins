25e2ac72260083588e565c8eb6b007e5d8848d5f
hsekhon24 <74080076+hsekhon24@users.noreply.github.com>
2021-02-18 13:53:38 -0800

    Added Custom Post Types

--- functions.php
+++ functions.php
@@ -184,3 +184,10 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 if ( class_exists( 'WooCommerce' ) ) {
 	require get_template_directory() . '/inc/woocommerce.php';
 }
+
+/**
+ * Custom Post types
+ */
+require get_template_directory() . '/inc/cpt.php';
+
+/**
