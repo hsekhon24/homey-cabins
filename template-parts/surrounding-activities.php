67b7f4f02ed1f03f1743a0b9b34b79dd3ce25bb8
Katerina-BCIT <73661411+Katerina-BCIT@users.noreply.github.com>
2021-03-08 15:58:31 -0800

    about page styles

--- template-parts/surrounding-activities.php
+++ template-parts/surrounding-activities.php
@@ -17,10 +17,12 @@
 
 		if( have_rows('surrounding_activities', 24) ): ?> <!--  24 is page_id of about page where we have this acf field -->
 		<h2> Surrounding Activities </h2>
-		
+
+
+	
 	<?php		// loop through the rows of data
 			while ( have_rows('surrounding_activities', 24) ) : the_row(); ?>
-
+				<div class="surrounding-activities-content">	
 					<h3>
 						<?php echo the_sub_field('activity_title'); ?>
 					</h3>
@@ -28,7 +30,7 @@
 		
 				<?php	$image = get_sub_field('activity_image');
 					if( !empty( $image ) ): ?>
-						<img src="<?php echo esc_url($image['sizes']['medium']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+						<img src="<?php echo esc_url($image['sizes']['thumbnail']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
 					<?php endif; ?>
 				
 				<p>
@@ -37,7 +39,7 @@
 
 					<a href="<?php echo get_sub_field('activity_link'); ?>"> More Info
 					</a>
-	
+				</div>
 		<?php	
 			endwhile;
 
